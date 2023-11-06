b066e701cdb32c3d34c29b6080869dc3f8fcdb10
Mike Donnalley <mdonnalley@salesforce.com>
2023-11-06 11:26:23 -0700

    fix: use node executable

--- bin/dev.js
+++ bin/dev.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env ts-node
+#!/usr/bin/env node --loader ts-node/esm --no-warnings=ExperimentalWarning
 // eslint-disable-next-line node/shebang
 async function main() {
   const {execute} = await import('@oclif/core')
